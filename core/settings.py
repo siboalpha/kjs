5db7e0ca852648eb18cc6221d147e817922937da
siboalpha <sibo.alphonsee@gmail.com>
2022-09-22 13:41:06 +0200

    added a requirement.txt file

--- core/settings.py
+++ core/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 load_dotenv()
+#load_dotenv("/home/admin/site/DjangoApp/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
